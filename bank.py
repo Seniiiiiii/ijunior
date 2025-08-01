a05341b75f88033c1669e4e5eee3a481f68c2f52
Алексей <alexbogdanov20@bk.ru>
2025-08-01 18:20:32 +0500

    New

--- bank.py
+++ bank.py
@@ -4,27 +4,40 @@ class BankAccount:
 
 
     def deposit(self, amount):
-        if amount > 0:
-            self.__balance += amount
-
+        try:
+            amount = int(amount)
+            if amount > 0:
+                self.__balance += amount
+            else:
+                print('Не верное значение!')
+        except ValueError:
+            print('Не верное значение!')
 
     def withdraw(self, amount):
-        if 0 < amount <= self.__balance:
-            self.__balance -= amount
+        try:
+            amount = int(amount)
 
+            if 0 < amount <= self.__balance:
+                self.__balance -= amount
+            else:
+                print('Не верное значение!')
+
+        except ValueError:
+            print('Не верное значение!')
 
     def get_balance(self):
         return self.__balance
 
-MENU = {
-    '1.': 'Пополнить депозит',
-    '2.': 'Снять депозит',
-    '3.': 'Посмотреть баланс',
-    '0.': 'Выход'
-}
 
 def main():
     my_account = BankAccount(100)
+
+    MENU = {
+        '1.': 'Пополнить депозит',
+        '2.': 'Снять депозит',
+        '3.': 'Посмотреть баланс',
+        '0.': 'Выход'
+    }
 
     while True:
         for key, value in MENU.items():
@@ -33,10 +46,10 @@ def main():
         user_input = input('\nВведите номер для управления меню: ')
 
         if user_input == "1":
-            amount = int(input('Введите сумму пополнения: '))
+            amount = input('Введите сумму пополнения: ')
             my_account.deposit(amount)
         elif user_input == "2":
-            amount = int(input('Введите сумму снятия: '))
+            amount = input('Введите сумму снятия: ')
             my_account.withdraw(amount)
         elif user_input == "3":
             print(f'На вашем депозите: {my_account.get_balance()}')
