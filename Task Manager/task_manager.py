32d2516448e1693c1f6317472e1404b493da20ca
Алексей <alexbogdanov20@bk.ru>
2025-08-28 21:46:58 +0500

    New

--- Task Manager/task_manager.py
+++ Task Manager/task_manager.py
@@ -3,34 +3,38 @@ import json
 class TaskManager:
     def __init__(self):
         self.tasks = [
-            {"описание": "Первая задача", "статус": False},
-            {"описание": "Вторая задача", "статус": False}
+            {"description": "Первая задача", "status": False},
+            {"description": "Вторая задача", "status": False}
         ]
 
     def add_task(self, description: str):
-        self.tasks.append({"описание": description, "статус": False})
+        self.tasks.append({"description": description, "status": False})
 
     def complete_task(self, index: int):
-        if index <= len(self.tasks):
+        if 0 <= index <= len(self.tasks):
             if self.tasks[index]:
-                self.tasks[index]['статус'] = True
+                self.tasks[index]['status'] = True
         else:
             print('такого в индекса нет')
 
     def remove_task(self, index: int):
-        if index <= len(self.tasks):
+        if 0 <= index <= len(self.tasks):
             self.tasks.pop(index)
         else:
             print('такого в индекса нет')
 
-    def save_to_json(self, filename: str):
-        with open('self.tasks.json', 'w') as filename:
-            json.dump(self.tasks, filename, indent=4)
+    def save_to_json(self, file: str):
+        my_json = 'self.tasks.json'
+        with open(my_json, 'w') as file:
+            json.dump(self.tasks, file, indent=4)
 
-    def load_from_json(self, filename: str):
-        with open('self.tasks.json', 'r') as filename:
-            self.tasks = json.load(filename)
+    def load_from_json(self, file: str):
+        my_json = 'self.tasks.json'
+        with open(my_json, 'r') as file:
+            self.tasks = json.load(file)
 
     def view_list(self):
         for index, item in enumerate(self.tasks):
-            print(f'{index}.{item['описание']} - Статус: {item['статус']}')
+            print(f'{index}.{item['description']} - Статус: {item['status']}')
+
+test = TaskManager()
